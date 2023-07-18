8111b5a211813fdf84b7e5617af13eb2082342f0
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-18 07:51:44 +0000

    Create invoice table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,3 +10,13 @@ CREATE TABLE invoice_items (
 
 CREATE INDEX invoice_items_invoice_id_idx ON invoice_items(invoice_id);
 CREATE INDEX invoice_items_treatment_id_idx ON invoice_items(treatment_id);
+
+CREATE TABLE invoices (
+  id int GENERATED ALWAYS AS IDENTITY,
+  total_amount numeric(10,2),
+  generated_at timestamp(6),
+  payed_at timestamp(6),
+  medical_history_id int,
+  PRIMARY KEY (id)
+);
+CREATE INDEX invoices_medical_history_id_idx ON invoices(medical_history_id);
